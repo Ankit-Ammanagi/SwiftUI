73b2d5f20f5886048e7638d9e577bd271b200e14
Ankit Ammanagi <ankit.ammanagi@falabella.cl>
2026-05-28 13:12:36 +0530

    Initial Commit

---
+++ l1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  l1
+//
+//  Created by Ankit Ammanagi on 28/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ l1/l1App.swift
@@ -0,0 +1,17 @@
+//
+//  l1App.swift
+//  l1
+//
+//  Created by Ankit Ammanagi on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct l1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
