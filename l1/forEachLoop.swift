938b875b444f088a53276a0dd9df6277398f0038
Ankit Ammanagi <ankit.ammanagi@falabella.cl>
2026-06-02 14:12:11 +0530

    Scroll view and lazy stack

--- l1/forEachLoop.swift
+++ l1/forEachLoop.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct forEachLoop: View {
-    let data: [String] = [
-        "Ankit",
-        "Aryan",
-        "Tushar",
-        "Chaitra",
-        "Reddy"
-    ]
     var body: some View {
-        VStack(spacing: 10){
-            ForEach(data.indices, id: \.self) { index in
-                Text("\(data[index]) : \(index+1)")
+        
+//        VStack(spacing: 10){
+//            ForEach(data.indices, id: \.self) { index in
+//                Text("\(data[index]) : \(index+1)")
+//            }
+//        }
+        
+        ScrollView{
+            ForEach(1..<50) { _ in
+                ScrollView(.horizontal){
+                    LazyHStack{
+                        ForEach(0..<100) { idx in
+                            Rectangle()
+                                .frame(width: 300, height: 200)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
             }
         }
+        .padding()
+        .scrollIndicators(.hidden ,axes: .vertical)
     }
 }
 
