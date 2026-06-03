1a812976f7c7abf9dfb804167b66f13a12a85a06
Ankit Ammanagi <ankit.ammanagi@falabella.cl>
2026-06-03 14:52:24 +0530

    Animation timelines

--- l1/AnimationTimelines.swift
+++ l1/AnimationTimelines.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct AnimationTimelines: View {
+    @State var isAnimating: Bool = false
+    let time: Double = 1
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Button("Animate") {
+                isAnimating.toggle()
+            }
+            .font(.largeTitle)
+            
+            Spacer()
+            
+            VStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: isAnimating ? 300 : 50,height: 100)
+                    .animation(
+                        .spring(
+                            response: 0.5,
+                            dampingFraction: 0.2
+                        ), value: isAnimating)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: isAnimating ? 300 : 50,height: 100)
+                    .animation(.linear(duration: time), value: isAnimating)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: isAnimating ? 300 : 50,height: 100)
+                    .animation(.easeIn(duration: time), value: isAnimating)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: isAnimating ? 300 : 50,height: 100)
+                    .animation(.easeInOut(duration: time), value: isAnimating)
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: isAnimating ? 300 : 50,height: 100)
+                    .animation(.easeOut(duration: time), value: isAnimating)
+            }
+            
+            Spacer()
+        }
     }
 }
 
